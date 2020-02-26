c83eec77eb367fd41dde1979e91bc341b26758fb
patgray210 <pngray@uno.edu>
2020-02-26 12:27:37 -0600

    wokring on js

--- Scripts/contact.js
+++ Scripts/contact.js
@@ -3,3 +3,30 @@ button.addEventListener("click",buttonEvent);
 function buttonEvent(){
     $('#contactForm').fadeToggle();
 }
+
+let button1  = document.getElementById("sumButton");
+button1.addEventListener("click",buttonEvent1);
+function buttonEvent1(){
+    $('#p1-sum').fadeToggle();
+    $('#p1').fadeToggle();
+}
+let button2  = document.getElementById("sumButton1");
+button2.addEventListener("click",buttonEvent1);
+
+let button3  = document.getElementById("sumButton2");
+button3.addEventListener("click",buttonEvent2);
+function buttonEvent2(){
+    $('#p2-sum').fadeToggle();
+    $('#p2').fadeToggle();
+}
+let button4  = document.getElementById("sumButton3");
+button4.addEventListener("click",buttonEvent2);
+
+let button5  = document.getElementById("sumButton4");
+button5.addEventListener("click",buttonEvent3);
+function buttonEvent3(){
+    $('#p3-sum').fadeToggle();
+    $('#p3').fadeToggle();
+}
+let button6  = document.getElementById("sumButton5");
+button6.addEventListener("click",buttonEvent3);
