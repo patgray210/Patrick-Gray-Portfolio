00bf05f666d0ba2677df3e7113096d0328c2a218
Patrick Gray <patgray210@gmail.com>
2021-07-30 14:28:53 -0500

    changed sql connection to be more secure

--- links/vb.php
+++ links/vb.php
@@ -4,22 +4,50 @@ $t=time();
 echo($t . "<br>");
 echo (date("Y-m-d",$t));
 
-   $con=mysqli_connect("127.0.0.1","root","Billybob1","event_crawler");
-
-   if (mysqli_connect_errno($con)) {
-      echo "Failed to connect to MySQL: " . mysqli_connect_error();
-   }
+   include_once('connection.php');
    $username = $_GET['$username'];
    $password = $_GET['$password'];
    $result = mysqli_query($con,"SELECT *  FROM events");
-   $row = mysqli_fetch_array($result);
-foreach($row as $value){
-	echo $value;
-}
-   $data = $row[0];
+ #  $row = mysqli_fetch_array($result);
+#foreach($row as $value){
+#	echo $value;
+#}
+   #$data = $row[0];
 
-   if($data){
-      echo $data;
-   }
-   mysqli_close($con);
+   #if($data){
+   #   echo $data;
+  # }
 ?>
+
+<!DOCTYPE html>
+<html>
+   <body>
+      <table> 
+         <tr>
+            <th>page</th>
+            <th>title</th>
+            <th>description</th>
+            <th>date</th>
+            <th>place</th>
+            <th>going</th>
+         </tr>
+         <?php 
+         while($rows=mysqli_fetch_array($result))
+         {
+         ?>
+            <tr>
+               <td> <?php echo $rows['page']; ?> </td>
+               <td> <?php echo $rows['title']; ?> </td>
+               <td> <?php echo $rows['description']; ?> </td>
+               <td> <?php echo $rows['date']; ?> </td>
+               <td> <?php echo $rows['place']; ?> </td>
+               <td> <?php echo $rows['going']; ?> </td>
+            </tr>
+            <?php
+         }
+   mysqli_close($con);
+
+         ?>
+      </table>
+   </body>
+</html>
